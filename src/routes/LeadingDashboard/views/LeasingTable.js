9d0ff88e7197a84c73723e1918bfec3c4b02ef71
Antonova Ekaterina <antonova.ekaterina19@yandex.ru>
2019-09-18 15:12:27 +0700

    LeasingTableRow in separate file

--- src/routes/LeadingDashboard/views/LeasingTable.js
+++ src/routes/LeadingDashboard/views/LeasingTable.js
@@ -1,52 +1,10 @@
 import React from 'react';
 import './LeasingTable.scss'
-import LeasingSlider from './LeasingTableSlider';
-import Favorites from './LeasingTableFavorites';
-import MiningShare from './LeasingTableMiningShare';
-import LeasersPayouts from './LeasingTablePayouts';
+import LeasingTableRow from './LeasingTableRow';
 import Button from 'yii-steroids/ui/form/Button';
 
 import {html} from 'components';
 const bem = html.bem('LeasingTable');
-
-
-class LeasingTableRow extends React.PureComponent {
-    constructor(props) {
-        super(props);
-    }
-
-    render() {
-
-        const { leaser: { address, website, miningShare, leasersPayouts, myLeasing }, index } = this.props;
-        const num = index + 1;
-
-        return (
-            <div className={bem.element('tr')}>
-                <div className={bem.element('td', 'num')}>
-                    {num}
-                </div>
-                <div className={bem.element('td', 'favorites')}>
-                    <Favorites />
-                </div>
-                <div className={bem.element('td', 'address')}>
-                    {address}
-                </div>
-                <div className={bem.element('td', 'site')}>
-                    {website}
-                </div>
-                <div className={bem.element('td', 'mining')}>
-                    <MiningShare {...miningShare}/>
-                </div>
-                <div className={bem.element('td', 'payouts')}>
-                    <LeasersPayouts {...leasersPayouts}/>
-                </div>
-                <div className={bem.element('td', 'leasing')}>
-                    <LeasingSlider myLeasing={myLeasing}/>
-                </div>
-            </div>
-        )
-    }
-}
 
 
 export default class LeasingTable extends React.PureComponent {
@@ -68,7 +26,6 @@ export default class LeasingTable extends React.PureComponent {
             </div>
         );
     }
-
 
     render() {
 
---
+++ src/routes/LeadingDashboard/views/LeasingTableRow.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import Favorites from './LeasingTableFavorites';
+import MiningShare from './LeasingTableMiningShare';
+import LeasersPayouts from './LeasingTablePayouts';
+import LeasingSlider from './LeasingTableSlider';
+
+import './LeasingTableRow.scss';
+
+import {html} from 'components';
+const bem = html.bem('LeasingTableRow');
+
+export default class LeasingTableRow extends React.PureComponent {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const { leaser: { address, website, miningShare, leasersPayouts, myLeasing }, index } = this.props;
+        const num = index + 1;
+
+        return (
+            <div className={bem.block()}>
+                <div className={bem.element('td', 'num')}>
+                    {num}
+                </div>
+                <div className={bem.element('td', 'favorites')}>
+                    <Favorites />
+                </div>
+                <div className={bem.element('td', 'address')}>
+                    {address}
+                </div>
+                <div className={bem.element('td', 'site')}>
+                    {website}
+                </div>
+                <div className={bem.element('td', 'mining')}>
+                    <MiningShare {...miningShare}/>
+                </div>
+                <div className={bem.element('td', 'payouts')}>
+                    <LeasersPayouts {...leasersPayouts}/>
+                </div>
+                <div className={bem.element('td', 'leasing')}>
+                    <LeasingSlider myLeasing={myLeasing}/>
+                </div>
+            </div>
+        )
+    }
+}
