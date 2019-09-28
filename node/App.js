78dceba56f7e9ab152bf2689d06ab944e2fc6f1e
CryptoBadBoy <raiman.elshan@gmail.com>
2019-09-29 00:18:45 +0300

    fix env USDNB_USDN

--- node/App.js
+++ node/App.js
@@ -30,7 +30,7 @@ module.exports = class App {
         }
         this.redisNamespace = process.env.REDIS_NAMESPACE || 'nt';
         this.dApps = {
-            [PairsEnum.USDNB_USDN]: process.enUSDNB_USDNv.APP_ADDRESS_USDNB_USDN || '3MrtHeXquGPcRd3YjJQHfY1Ss6oSDpfxGuL', // testnet
+            [PairsEnum.USDNB_USDN]: process.env.APP_ADDRESS_USDNB_USDN || '3MrtHeXquGPcRd3YjJQHfY1Ss6oSDpfxGuL', // testnet
             [PairsEnum.EURNB_EURN]: process.env.APP_ADDRESS_EURNB_EURN || '3Mz5Ya4WEXatCfa2JKqqCe4g3deCrFaBxiL', // testnet
         };
 
