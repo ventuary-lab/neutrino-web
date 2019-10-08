50e08cae5ef825f543fb32c75071fb44592fd443
looktoroot <>
2019-10-08 17:27:31 +0700

    add test rpd contract

--- node/App.js
+++ node/App.js
@@ -30,8 +30,8 @@ module.exports = class App {
         }
         this.redisNamespace = process.env.REDIS_NAMESPACE || 'nt';
         this.dApps = {
-            [PairsEnum.USDNB_USDN]: process.env.APP_ADDRESS_USDNB_USDN || '3N4Pj4MutKVgrmcuX7jgyVGWoBhDyKYFZBj', // testnet
-            // [PairsEnum.USDNB_USDN]: process.env.APP_ADDRESS_USDNB_USDN || '3NAXNEjQCDj9ivPGcdjkRhVMBkkvyGRUWKm', // testnet for rpd
+            // [PairsEnum.USDNB_USDN]: process.env.APP_ADDRESS_USDNB_USDN || '3N4Pj4MutKVgrmcuX7jgyVGWoBhDyKYFZBj', // testnet
+            [PairsEnum.USDNB_USDN]: process.env.APP_ADDRESS_USDNB_USDN || '3NAXNEjQCDj9ivPGcdjkRhVMBkkvyGRUWKm', // testnet for rpd
             [PairsEnum.EURNB_EURN]: process.env.APP_ADDRESS_EURNB_EURN || '3Mz5Ya4WEXatCfa2JKqqCe4g3deCrFaBxiL', // testnet
         };
 
