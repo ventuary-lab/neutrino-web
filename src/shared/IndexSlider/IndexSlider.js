25ea3da60b6af20b5710ed823081ec0db31c3552
CryptoBadBoy <raiman.elshan@gmail.com>
2019-10-10 01:04:09 +0300

    fix text

--- src/shared/IndexSlider/IndexSlider.js
+++ src/shared/IndexSlider/IndexSlider.js
@@ -87,10 +87,10 @@ export default class IndexSlider extends React.Component {
                     {__('01. The concept')}
                 </div>
                 <h3 className={bem.element('slide-title')}>
-                    {__('What is a Collateralized Debt Position (CDP)?')}
+                    {__('What is a collateralised stable coin or synthetic asset?')}
                 </h3>
                 <p className={bem.element('slide-text')}>
-                    {__('A CDP enables the generation of USD-N stablecoins against the collateral (currently WAVES) that are locked up in the CDP until the generated USD-Ns are paid back.')}
+                    {__('It is a token on the Waves platform whose capitalization and price stability is ensured by WAVES tokens reserved on the smart contract.')}
                 </p>
             </div>,
             <div className={bem.element('slide-inner')}>
@@ -98,10 +98,10 @@ export default class IndexSlider extends React.Component {
                     {__('02. The benefits')}
                 </div>
                 <h3 className={bem.element('slide-title')}>
-                    {__('What are the benefits of opening a CDP?')}
+                    {__('What are the benefits of minting and staking neutrino based assets?')}
                 </h3>
                 <p className={bem.element('slide-text')}>
-                    {__('Users can derive liquidity by generating USD-N stablecoins without giving up ownership of the collateral, as long as they ensure that the CDP holds enough collateral to cover the value of the USD-N stablecoins. When using WAVES as collateral, the value of the locked up collateral must always be more than 150% of the amount of the USD-N stablecoins that were generated.')}
+                    {__('You can mitigate the volatility risk of the WAVES token and receive income in stable coins or synthetic assets through leasing.')}
                 </p>
             </div>,
             <div className={bem.element('slide-inner')}>
@@ -109,32 +109,10 @@ export default class IndexSlider extends React.Component {
                     {__('03. The setup')}
                 </div>
                 <h3 className={bem.element('slide-title')}>
-                    {__('How does it work?')}
+                    {__('How does one work with Neutrino?')}
                 </h3>
                 <p className={bem.element('slide-text')}>
-                    {__('First, you determine the amount of WAVES to lock up in the CDP. Some USD-N stablecoins are generated against the locked up WAVES. The user can spend those USD-Ns at their discretion. Finally, you pay back the USD-N stablecoins together with a stability fee when you no longer need the liquidity, and then you can withdraw the collateral that you locked up.')}
-                </p>
-            </div>,
-            <div className={bem.element('slide-inner')}>
-                <div className={bem.element('slide-sub-title')}>
-                    {__('04. The risks')}
-                </div>
-                <h3 className={bem.element('slide-title')}>
-                    {__('Is there any risk involved in creating a CDP?')}
-                </h3>
-                <p className={bem.element('slide-text')}>
-                    {__('As long as you monitor your CDP and ensure that the value of the locked up WAVES is always more that 150% of the USD-N stablecoins that you have generated, your CDP will not be liquidated. If the value of the collateral approaches 150%, you can add more collateral or pay back some of the debt. If the value of the locked up collateral fails below 150%, then your CDP will be liquidated. This means that your collateral is being sold automatically to cover the value of the USD-N stablecoins that you generated. Any remaining collateral is returned to your CDP and made available for withdrawal. See the Terms of Service for risks involved.')}
-                </p>
-            </div>,
-            <div className={bem.element('slide-inner')}>
-                <div className={bem.element('slide-sub-title')}>
-                    {__('05. The cost')}
-                </div>
-                <h3 className={bem.element('slide-title')}>
-                    {__('Does it cost anything?')}
-                </h3>
-                <p className={bem.element('slide-text')}>
-                    {__('There is a stability fee on the USD-N stablecoins of 18.50% per year. You pay the fee with USD-NB (bonds) when you pay back the USD-N. If your CDP is liquidated, there is a 13% liquidation penalty subtracted when the locked collateral is sold.')}
+                    {__('Send WAVES to the smart contract using the Neutrino dashboard and receive a Neutrino-based asset back. Buy bonds in an open auction and make a profit from their liquidation in the Bonds dashboard.')}
                 </p>
             </div>
         ];
