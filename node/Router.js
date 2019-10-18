64938999cbd3fe90a0ff333bffa60bed81d7ca04
CryptoBadBoy <raiman.elshan@gmail.com>
2019-10-18 13:38:46 +0300

    fix rpd index

--- node/Router.js
+++ node/Router.js
@@ -73,7 +73,7 @@ module.exports = class Router {
                             break;
                         
                         historySyncIndex = balanceHistory[i]
-                        historyElementIndex = index
+                        historyElementIndex = i
 
                         if(balanceHistory[i] == index) 
                             break;
