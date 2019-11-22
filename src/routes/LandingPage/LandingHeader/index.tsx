becba08ee176ab565e9a9971428c52f50a6154bd
Shamil Khalilov <shamfama@gmail.com>
2019-11-22 21:02:20 +0300

    Header dropdown rework

--- src/routes/LandingPage/LandingHeader/index.tsx
+++ src/routes/LandingPage/LandingHeader/index.tsx
@@ -13,12 +13,26 @@ interface Link {
 }
 
 class LandingHeader extends React.Component<Props> {
+    productLinks!: Link[];
+    links!: Link[];
+
     constructor(props) {
         super(props);
-    }
 
-    render() {
-        const links = [
+        this.mapLink = this.mapLink.bind(this);
+
+        this.productLinks =  [
+            {
+                label: 'Neutrino dashboard',
+            },
+            {
+                label: 'Bonds dashboard',
+            },
+            {
+                label: 'Staking dashboard',
+            },
+        ];
+        this.links = [
             {
                 label: 'Products',
             },
@@ -28,19 +42,31 @@ class LandingHeader extends React.Component<Props> {
             {
                 label: 'Login',
             },
-        ].map((link: Link) => (
+        ];
+    }
+
+    mapLink(link: Link) {
+        return (
             <li>
                 <a href="#">{link.label}</a>
             </li>
-        ));
+        );
+    }
+
+    render() {
+        const links = this.links.map(this.mapLink);
+        const productLinks = this.productLinks.map(this.mapLink);
 
         return (
             <div className={bem.element('main')}>
                 <div className={bem.element('logo')}>
-                    <img src={mainLogo} alt="" />
+                    <img src={mainLogo} alt="neutrino" />
                     <span>beta</span>
                 </div>
                 <div className={bem.element('actions')}>
+                    <div className={bem.element('products-dp')}>
+                        <ul>{productLinks}</ul>
+                    </div>
                     <ul className={bem.element('links')}>{links}</ul>
                 </div>
             </div>
