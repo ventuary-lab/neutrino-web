fd188e6776c1e77b6a171705dfe7e51328c01b5f
Shamil Khalilov <shamfama@gmail.com>
2019-12-01 14:39:39 +0300

    Adjust dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,14 @@
 FROM node:12-alpine
 
-COPY package.json yarn.lock .babelrc .env.dev .env.example tsconfig.json /app/
+COPY package.json yarn.lock \
+.babelrc .env.dev .env.example \
+tsconfig.json tsconfig-node.json /app/
+
 WORKDIR /app
 
 COPY node /app/node
 COPY src /app/src
+COPY src/static /app/public
 COPY webpack.js /app/webpack.js
 COPY server.js /app/server.js
 
@@ -12,4 +16,4 @@ RUN yarn install
 RUN npm install ts-node -g
 RUN npm run build
 
-ENTRYPOINT [ "ts-node", "--log-error", "server.js" ]
+ENTRYPOINT [ "ts-node", "--project", "tsconfig-node.json", "--log-error", "server.js" ]
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import dynamic from 'next/dynamic';
 
-// import LandingPage from '../src/routes/LandingPage';
 import 'style/index.scss';
-import './static/images/favicon.ico';
+import 'static/images/favicon.ico';
 import 'shared/Layout/Layout.scss';
 
 const App: React.FC = () => {
-    const DynamicLandingPage = dynamic(() => {
-        return typeof window === 'undefined' ? null : import('../src/routes/LandingPage')
-    }, { ssr: true });
+    const DynamicLandingPage = dynamic(() => import('../src/routes/LandingPage'), { ssr: true });
 
     return (
         <div className='Layout'>
