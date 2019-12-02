f7265a3445d598839842d6225bf9939492f48241
Shamil Khalilov <shamfama@gmail.com>
2019-12-02 17:37:06 +0300

    Add fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import dynamic from 'next/dynamic';
+let routes, ROUTE_ROOT;
+
+if (typeof window !== 'undefined') {
+    const rs = require('routes');
+    routes = rs.default;
+    ROUTE_ROOT = rs.ROUTE_ROOT;
+}
 
 import 'style/index.scss';
 import 'static/images/favicon.ico';
 import 'shared/Layout/Layout.scss';
 
 const App: React.FC = () => {
-    const DynamicLandingPage = dynamic(() => import('../src/routes/LandingPage'), { ssr: false });
+    const Layout = dynamic(() => import('../src/routes/LandingPage'), { ssr: true });
 
-    return (
-        <div className='Layout'>
-            <div></div>
-            <DynamicLandingPage />
-        </div>
-    )
+    return routes && ROUTE_ROOT ? <Layout page={routes[ROUTE_ROOT]} /> : <div style={{ display: 'none' }}>I am empty :(</div>;
 };
 
 export default App;
