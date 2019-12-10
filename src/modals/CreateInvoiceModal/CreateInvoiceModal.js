319c29a5994705096c8c27089c38e90095486737
Shamil Khalilov <shamfama@gmail.com>
2019-12-10 16:27:12 +0300

    Change url

--- src/modals/CreateInvoiceModal/CreateInvoiceModal.js
+++ src/modals/CreateInvoiceModal/CreateInvoiceModal.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {connect} from 'react-redux';
-import {getFormValues} from 'redux-form';
+import { connect } from 'react-redux';
+import { getFormValues } from 'redux-form';
 import Modal from 'yii-steroids/ui/modal/Modal';
+import { ROUTE_NEUTRINO } from 'routes';
+import CurrencyEnum from 'enums/CurrencyEnum';
 
-
-import {html} from 'components';
+import { html } from 'components';
 import CopyToClipboard from 'shared/CopyToClipboard';
 import TransferForm from 'shared/TransferForm';
-import {getPairName} from 'reducers/currency';
+import { getPairName } from 'reducers/currency';
 import validate from 'shared/validate';
 
 import './CreateInvoiceModal.scss';
@@ -16,16 +17,11 @@ import './CreateInvoiceModal.scss';
 const bem = html.bem('CreateInvoiceModal');
 const FORM_ID = 'CreateInvoiceModalForm';
 
-
-@connect(
-    state => ({
-        pairName: getPairName(state),
-        formValues: getFormValues(FORM_ID)(state),
-    })
-)
+@connect(state => ({
+    pairName: getPairName(state),
+    formValues: getFormValues(FORM_ID)(state),
+}))
 export default class CreateInvoiceModal extends React.PureComponent {
-
-
     static propTypes = {
         currency: PropTypes.string,
     };
@@ -41,7 +37,6 @@ export default class CreateInvoiceModal extends React.PureComponent {
     }
 
     render() {
-
         return (
             <Modal
                 {...this.props.modalProps}
@@ -59,10 +54,8 @@ export default class CreateInvoiceModal extends React.PureComponent {
                     </div>
                     {this.state.invoiceLink && (
                         <div className={bem.element('link-block')}>
-                            <span className={bem.element('link')}>
-                                {this.state.invoiceLink}
-                            </span>
-                            <CopyToClipboard copyText={this.state.invoiceLink}/>
+                            <span className={bem.element('link')}>{this.state.invoiceLink}</span>
+                            <CopyToClipboard copyText={this.state.invoiceLink} />
                         </div>
                     )}
                 </div>
@@ -72,15 +65,18 @@ export default class CreateInvoiceModal extends React.PureComponent {
 
     _onSubmit(address, amount) {
         validate(address, [
-            ['address', function (address) {
-                if (/^[A-Za-z0-9]{30,40}$/.test(address) === false) {
-                    return __('Recipient address is not valid');
-                }
-            }]
+            [
+                'address',
+                function(address) {
+                    if (/^[A-Za-z0-9]{30,40}$/.test(address) === false) {
+                        return __('Recipient address is not valid');
+                    }
+                },
+            ],
         ]);
 
-        const link = `${location.origin}?invoiceAddress=${address}&invoiceAmount=${amount}&invoiceCurrency=${this.props.currency}`;
+        const link = `${location.origin}/${ROUTE_NEUTRINO}/${CurrencyEnum.USD_N}?invoiceAddress=${address}&invoiceAmount=${amount}&invoiceCurrency=${this.props.currency}`;
 
-        this.setState({invoiceLink: link});
+        this.setState({ invoiceLink: link });
     }
 }
