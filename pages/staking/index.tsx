b9153e3f022aff067698319be533840a73241b5d
Shamil Khalilov <shamfama@gmail.com>
2019-12-17 00:41:02 +0300

    Adjust SSR

---
+++ pages/staking/index.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import axios from 'axios';
+import dynamic from 'next/dynamic';
+import Head from 'next/head';
+
+import 'style/index.scss';
+import 'shared/Layout/Layout.scss';
+
+interface AppConfig {
+    env: {
+        [key: string]: string;
+    };
+}
+const getGoogleTag = (config: AppConfig) => config.env.google_tag_id;
+
+interface State {
+    googleTagId?: string;
+}
+
+class LandingPage extends React.Component<{}, State> {
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+    }
+
+    async componentDidMount() {
+        try {
+            const res = await axios.get('/api/v1/init');
+
+            this.setState({ googleTagId: getGoogleTag(res.data.config) });
+        } catch (err) {
+            console.warn('Error occured on google tag id fetch');
+        }
+    }
+
+    render() {
+        const { googleTagId } = this.state;
+        const DynamicPage = dynamic(() => import('shared/Staking/StakingLanding'), { ssr: true });
+
+        return (
+            <div>
+                <Head>
+                    <link rel="icon" href={'static/images/favicon.ico'} />
+                </Head>
+                <div className="Layout">
+                    <div></div>
+                    <DynamicPage />
+                </div>
+                <script
+                    async
+                    src={`https://www.googletagmanager.com/gtag/js?id=${googleTagId}`}
+                ></script>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${googleTagId}');
+                    `,
+                    }}
+                />
+                <style jsx>{`
+                    background: #f1f1f1;
+                `}</style>
+            </div>
+        );
+    }
+}
+
+export default LandingPage;
