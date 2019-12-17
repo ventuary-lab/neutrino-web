b78cf3f6059affe177aa65302173d16d594e6942
Shamil Khalilov <shamfama@gmail.com>
2019-12-18 00:38:55 +0300

    Rework titles & pretty render

--- src/ui/global/helpers.ts
+++ src/ui/global/helpers.ts
@@ -5,8 +5,19 @@ export const onlyDecimalRegex2 = /^\d*\.?\d*$/;
 export const prettyPrintNumber = num => {
     let rs = num.toString();
     const regex = /(-?\d+)(\d{3})/;
+
     while (regex.test(rs)) {
         rs = rs.replace(regex, "$1,$2");
+    }
+    return rs;
+}
+
+export const omitThousandsNumber = num => {
+    let rs = num.toString();
+    const regex = /(-?\d+)(\d{3})/;
+
+    while (regex.test(rs)) {
+        rs = rs.replace(regex, "$1k");
     }
     return rs;
 }
