946a728edf85ea43525fb4595ccc3d6cce76bd08
Shamil Khalilov <shamfama@gmail.com>
2019-12-30 18:32:47 +0300

    Add long pulling

--- node/services/ExplorerApiService.ts
+++ node/services/ExplorerApiService.ts
@@ -1,24 +1,91 @@
-import { Request, Response } from 'express';
+import { Request, Response, response } from 'express';
+import { ApplicationParams } from '../types';
 import axios from 'axios';
 
 class ExplorerApiService {
     apiUrl: string;
+    contractApp: ApplicationParams;
+    proxyRoutesCollection: Set<string>;
+    storageKey: string;
+    updateFrequency: number;
+    updateInterval: NodeJS.Timeout;
 
-    constructor () {
+    constructor (contractApp: ApplicationParams) {
+        this.contractApp = contractApp;
 
         this.apiUrl = 'https://neutrinoexplorer.herokuapp.com/';
+
+        this.proxyRoutesCollection = new Set();
+        this.storageKey = 'explorer_api_cache';
+        this.updateFrequency = 3000;
     }
 
     async handleRequest (req: Request, res: Response) {
         const route = String(req.originalUrl).replace('/api/explorer', '/api');
 
-        try {
-            const resp = await axios.get(route, { baseURL: this.apiUrl });
+        if (this.proxyRoutesCollection.has(route)) {
+            const cachedValue = await this.contractApp.storage.hget(
+                this.storageKey,
+                route
+            );
 
-            res.send(JSON.stringify(resp.data));
-        } catch (err) {
-            res.send({ error: true, message: 'Not Found' });
+            res.send(cachedValue);
+        } else {
+            this.proxyRoutesCollection.add(route);
+
+            await this.update();
+
+            const value = await this.contractApp.storage.hget(
+                this.storageKey,
+                route
+            );
+
+            res.send(value);
         }
+    }
+
+    async requestApiValue (route: string) {
+        try {
+            const response = await axios.get<string>(route, { baseURL: this.apiUrl });
+
+            return response;
+        } catch (err) {
+            console.log(`Error occured on Explorer API call. Route: ${route}`, err);
+            return null;
+        }
+    }
+
+    async update () {
+        if (this.proxyRoutesCollection.size === 0) {
+            return;
+        } else {
+            for await (const route of this.proxyRoutesCollection.keys()) {
+                const response = await this.requestApiValue(route);
+
+                if (response !== null) {
+                    await this.contractApp.storage.hset(
+                        this.storageKey,
+                        route,
+                        `${response.data}`
+                    )
+                }
+            }
+        }
+    }
+
+    async startPulling () {
+        await this.contractApp.storage.del(this.storageKey);
+
+        this.updateInterval = setInterval(
+            async () => {
+                await this.update();
+            },
+            this.updateFrequency
+        )
+    }
+
+    async stopPulling () {
+        clearInterval(this.updateInterval);
     }
 }
 
