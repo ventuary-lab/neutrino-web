3d7615480e33a49fd4684780f304dac5da6f9764
Shamil Khalilov <shamfama@gmail.com>
2020-01-16 19:58:00 +0300

    Adjust stylesheet for login modal

--- src/modals/LoginTypeModal/index.tsx
+++ src/modals/LoginTypeModal/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import Modal from 'react-modal';
+import InlineSVG from 'svg-inline-react';
 import { html, dal } from 'components';
+import crossIcon from '!svg-inline-loader!static/images/landing/cross-icon.svg';
 import { wavesExchange as wavesExchangeLogo } from 'static/images/guide/companies';
-import wavesKeeperLogo from 'static/images/logos/waves-keeper.png';
+import wavesKeeperLogo from 'static/images/logos/waves-romb.svg';
 import { BlurContext } from 'shared/Layout/context';
 import { hasBooleanPropChanged } from 'shared/Layout/helpers';
 import { InstallKeeperModalContext } from 'shared/Layout/context';
@@ -18,6 +20,7 @@ interface Props {
     onClose: () => void;
 }
 interface ModalLink {
+    className?: string;
     label: string;
     img: string;
     handler: (keeperContext: IInstallKeeperModalContext) => void;
@@ -57,13 +60,13 @@ class LoginTypeModal extends React.Component<Props> {
 
     mapModalLink(item: ModalLink, keeperContext: IInstallKeeperModalContext) {
         return (
-            <a
-                href="#"
-                className={bem.element('login-item')}
-                onClick={() => item.handler(keeperContext)}
-            >
-                <img src={item.img} className={bem.element('login-item-img')} />
-                <span>{item.label}</span>
+            <a href="#" onClick={() => item.handler(keeperContext)}>
+                <div className={bem.element('login-item')}>
+                    <div className={item.className || ''}>
+                        <img src={item.img} className={bem.element('login-item-img')} />
+                    </div>
+                    <span>{item.label}</span>
+                </div>
             </a>
         );
     }
@@ -71,13 +74,14 @@ class LoginTypeModal extends React.Component<Props> {
     getLinks(keeperContext: IInstallKeeperModalContext) {
         const links: ModalLink[] = [
             {
-                label: 'Login using Waves Keeper',
+                label: 'Waves Keeper',
                 img: wavesKeeperLogo,
                 handler: () => this.onWavesKeeperLogin(keeperContext),
             },
             {
-                label: ' Login using Web Keeper',
+                label: 'Waves Signer',
                 img: wavesExchangeLogo,
+                className: bem.element('we-cont'),
                 handler: () => this.onWebKeeperLogin(keeperContext),
             },
         ];
@@ -97,6 +101,14 @@ class LoginTypeModal extends React.Component<Props> {
                     <InstallKeeperModalContext.Consumer>
                         {keeperContext => (
                             <div className={bem.element('main')}>
+                                <div className={bem.element('head')}>
+                                    <div
+                                        className={bem.element('head-icon')}
+                                        onClick={this.props.onClose}
+                                    >
+                                        <InlineSVG src={crossIcon} />
+                                    </div>
+                                </div>
                                 <div className={bem.element('list')}>
                                     {this.getLinks(keeperContext)}
                                 </div>
