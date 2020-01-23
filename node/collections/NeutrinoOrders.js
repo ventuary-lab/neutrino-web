b30027c0b5d602b40df01c025cda04489acd5938
Shamil Khalilov <shamfama@gmail.com>
2020-01-23 20:42:16 +0300

    Revert "Merge pull request #148 from ventuary-lab/fix-liquidation"
    This reverts commit 5e92d8648bead8b5bbbb2c556df7748bd46b6b02, reversing
    changes made to 13bd031d9e7962f7a7ddadfef568d2dc67cfe3b0.

--- node/collections/NeutrinoOrders.js
+++ node/collections/NeutrinoOrders.js
@@ -1,11 +1,11 @@
 const _orderBy = require('lodash/orderBy');
 
 const OrderTypeEnum = require('../enums/OrderTypeEnum');
-const OrderStatusEnum = require('../enums/OrderStatusEnum');
 const BaseCollection = require('../base/BaseCollection');
 const { mapFieldsToNumber } = require('./helpers');
 
 module.exports = class NeutrinoOrders extends BaseCollection {
+
     getKeys(id = '([A-Za-z0-9]{40,50})$') {
         return [
             `order_height_${id}`,
@@ -13,11 +13,7 @@ module.exports = class NeutrinoOrders extends BaseCollection {
             `order_total_${id}`,
             `order_filled_total_${id}`,
             `order_status_${id}`,
-            `order_prev_${id}`,
-            `order_next_${id}`,
             'orderbook',
-            'order_first',
-            'order_last',
         ];
     }
 
@@ -35,46 +31,27 @@ module.exports = class NeutrinoOrders extends BaseCollection {
      * @returns {Promise}
      */
     async getOpenedOrders() {
-        let orders = await this.getItemsAll();
+        let orders = await this.getOrders();
+        orders = orders
+            .filter(order => order.index !== null)
+            .map(order => mapFieldsToNumber(order, ['index']));
 
-        let sortedOrders = [];
-
-        orders = orders.filter(order => order.status == OrderStatusEnum.NEW);
-        if (orders == undefined || orders.length == 0) return orders;
-
-        let firstOrder = orders.filter(order => order.isFirst)[0];
-        if (firstOrder == undefined || firstOrder.length == 0) return orders;
-
-        let nextProcessOrder = firstOrder;
-        sortedOrders.push(firstOrder);
-        while (true) {
-            if (nextProcessOrder.orderNext == null) {
-                return sortedOrders;
-            }
-            let foundOrder = orders.filter(order => order.id == nextProcessOrder.orderNext)[0];
-            sortedOrders.push(foundOrder);
-            nextProcessOrder = foundOrder;
-        }
+        orders = _orderBy(orders, 'index', 'asc');
+        return orders;
     }
 
     async getUserOpenedOrders(address) {
         let orders = await this.getOrders();
-        return orders.filter(
-            order => order.owner === address && order.status === OrderStatusEnum.NEW
-        );
+        return orders.filter(order => order.owner === address && order.index !== null);
     }
 
     async getUserHistoryOrders(address) {
         let orders = await this.getOrders();
-        return orders.filter(
-            order => order.owner === address && order.status !== OrderStatusEnum.NEW
-        );
+        return orders.filter(order => order.owner === address && order.index === null);
     }
 
     async _prepareItem(id, item) {
-        const orderNext = item['order_next_' + id] || null;
-        const orderPrev = item['order_prev_' + id] || null;
-
+        const index = item.orderbook.split('_').filter(Boolean).indexOf(id);
         const height = item['order_height_' + id];
         const total = item['order_total_' + id] || 0;
         const filledTotal = item['order_filled_total_' + id] || 0;
@@ -86,11 +63,8 @@ module.exports = class NeutrinoOrders extends BaseCollection {
             status: item['order_status_' + id],
             total,
             restTotal: total - filledTotal,
+            index: index !== -1 ? index : null,
             type: OrderTypeEnum.LIQUIDATE,
-            orderNext,
-            orderPrev,
-            isFirst: id == item.order_first,
-            isLast: id == item.order_last,
         };
     }
 
@@ -100,4 +74,5 @@ module.exports = class NeutrinoOrders extends BaseCollection {
             id,
         };
     }
+
 };
--- src/routes/BondsDashboard/views/OrderBook.js
+++ src/routes/BondsDashboard/views/OrderBook.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { html } from 'components';
-import { round as _round, sum as _sum, groupBy as _groupBy, orderBy as _orderBy } from 'lodash';
+import {
+    round as _round,
+    sum as _sum,
+    groupBy as _groupBy,
+    orderBy as _orderBy,
+} from 'lodash';
 import { computeROIForOrder } from './helpers';
 
 import './OrderBook.scss';
@@ -34,103 +39,21 @@ export default class OrderBook extends React.PureComponent {
         super(props);
 
         this.computeROIForField = this.computeROIForField.bind(this);
-        this.getBuyBondOrders = this.getBuyBondOrders.bind(this);
-        this.getLiquidateOrders = this.getLiquidateOrders.bind(this);
     }
 
     computeROIForField(groupedField) {
         const { controlPrice } = this.props;
 
         return _round(
-            _sum(groupedField.map(order => computeROIForOrder(order, controlPrice))) /
-                groupedField.length,
+            _sum(
+                groupedField.map(order => computeROIForOrder(order, controlPrice))
+            ) / groupedField.length,
             2
         );
     }
 
-    getLiquidateOrders({ formTab, orders, user }) {
-        const groupedByAddress = _groupBy(orders, 'owner');
-        const ownerKeys = Object.keys(groupedByAddress);
-
-        return (
-            formTab === 'liquidate' && (
-                <div className={bem.element('columns')}>
-                    {ownerKeys.map(ownerKey => {
-                        const orders = groupedByAddress[ownerKey];
-                        const aggregatedOrder = orders.reduce(
-                            (accum, iter) => ({
-                                ...accum,
-                                restTotal: (accum.restTotal || 0) + iter.restTotal,
-                            }),
-                        );
-
-                        return (
-                            <div
-                                key={aggregatedOrder.id}
-                                className={bem.element('body-row', {
-                                    my: user && user.address === ownerKey,
-                                })}
-                            >
-                                <div className={bem.element('body-column', 'bg')}>
-                                    {_round(aggregatedOrder.restTotal)}
-                                </div>
-                                <div className={bem.element('body-column', 'address')}>
-                                    {ownerKey}
-                                </div>
-                            </div>
-                        );
-                    })}
-                </div>
-            )
-        );
-    }
-
-    getBuyBondOrders({ orders, formTab, user }) {
-        const groupedOrders = _groupBy(orders, 'price');
-        const sortedKeys = _orderBy(
-            Object.keys(groupedOrders).map(item => Number(item)),
-            null,
-            'desc'
-        );
-
-        return (
-            formTab === 'buy' && (
-                <div className={bem.element('columns')}>
-                    {sortedKeys.map(price => (
-                        <div
-                            key={price}
-                            className={bem.element('body-row', {
-                                my:
-                                    user &&
-                                    groupedOrders[price]
-                                        .map(order => order.owner)
-                                        .includes(user.address),
-                            })}
-                        >
-                            <div className={bem.element('body-column', 'bg')}>
-                                {_round(_sum(groupedOrders[price].map(order => order.restAmount)))}
-                            </div>
-                            <div className={bem.element('body-column', 'bg')}>
-                                {this.computeROIForField(groupedOrders[price])}
-                            </div>
-                            <div className={bem.element('body-column')}>
-                                {_round(price / 100, 2)}
-                            </div>
-                            <div className={bem.element('body-column', 'bg')}>
-                                {_round(
-                                    _sum(groupedOrders[price].map(order => order.restTotal)),
-                                    2
-                                )}
-                            </div>
-                        </div>
-                    ))}
-                </div>
-            )
-        );
-    }
-
     render() {
-        const { orders, controlPrice, formTab, user, baseCurrency } = this.props;
+        const { orders, controlPrice } = this.props;
 
         if (!orders) {
             return null;
@@ -138,7 +61,7 @@ export default class OrderBook extends React.PureComponent {
 
         const headerRow = (
             <div className={bem.element('header-row', 'summary')}>
-                {formTab === 'buy' && (
+                {this.props.formTab === 'buy' && (
                     <>
                         <div className={bem.element('header-column', 'upper-case')}>
                             {_round(_sum(orders.map(order => order.restAmount)))}
@@ -154,7 +77,7 @@ export default class OrderBook extends React.PureComponent {
                         </div>
                     </>
                 )}
-                {formTab === 'liquidate' && (
+                {this.props.formTab === 'liquidate' && (
                     <>
                         <div className={bem.element('header-column', 'upper-case')}>
                             {_round(_sum(orders.map(order => order.restTotal)))}
@@ -163,6 +86,9 @@ export default class OrderBook extends React.PureComponent {
                 )}
             </div>
         );
+        
+        let groupedOrders = _groupBy(orders, 'price');
+        const sortedKeys = _orderBy(Object.keys(groupedOrders).map(item => Number(item)), null, 'desc');
 
         const wavesByUsdAmount = _round(controlPrice / 100, 2);
         const usdnByWavesAmount = _round(1 / wavesByUsdAmount, 2);
@@ -178,9 +104,10 @@ export default class OrderBook extends React.PureComponent {
                 </div>
                 <div className={bem.element('header-row')}>
                     <div className={bem.element('header-column', 'upper-case')}>
-                        {CurrencyEnum.getLabel(baseCurrency)}
+                        {CurrencyEnum.getLabel(this.props.baseCurrency)}
                     </div>
-                    {formTab === 'buy' && (
+
+                    {this.props.formTab === 'buy' && (
                         <>
                             <div className={bem.element('header-column', 'upper-case')}>ROI</div>
                             <div className={bem.element('header-column')}>{__('Price')}</div>
@@ -191,8 +118,59 @@ export default class OrderBook extends React.PureComponent {
                     )}
                 </div>
                 {headerRow}
-                {this.getBuyBondOrders({ formTab, user, orders })}
-                {this.getLiquidateOrders({ formTab, user, orders })}
+                {this.props.formTab === 'buy' && (
+                    <div className={bem.element('columns')}>
+                        {sortedKeys.map(price => (
+                            <div
+                                key={price}
+                                className={bem.element('body-row', {
+                                    my:
+                                        this.props.user &&
+                                        groupedOrders[price]
+                                            .map(order => order.owner)
+                                            .includes(this.props.user.address),
+                                })}
+                            >
+                                <div className={bem.element('body-column', 'bg')}>
+                                    {_round(
+                                        _sum(groupedOrders[price].map(order => order.restAmount))
+                                    )}
+                                </div>
+                                <div className={bem.element('body-column', 'bg')}>
+                                    {this.computeROIForField(groupedOrders[price])}
+                                </div>
+                                <div className={bem.element('body-column')}>
+                                    {_round(price / 100, 2)}
+                                </div>
+                                <div className={bem.element('body-column', 'bg')}>
+                                    {_round(
+                                        _sum(groupedOrders[price].map(order => order.restTotal)),
+                                        2
+                                    )}
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                )}
+                {this.props.formTab === 'liquidate' && (
+                    <div className={bem.element('columns')}>
+                        {this.props.orders.map(order => (
+                            <div
+                                key={order.id}
+                                className={bem.element('body-row', {
+                                    my: this.props.user && this.props.user.address === order.owner,
+                                })}
+                            >
+                                <div className={bem.element('body-column', 'bg')}>
+                                    {_round(order.restTotal)}
+                                </div>
+                                <div className={bem.element('body-column', 'address')}>
+                                    {order.owner}
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                )}
             </div>
         );
     }
