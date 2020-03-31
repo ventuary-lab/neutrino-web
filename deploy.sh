cb6cde4952dc1fbb5030f4ca157d3f3329b81665
Shamil Khalilov <shamfama@gmail.com>
2020-03-31 23:50:32 +0300

    Fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -24,7 +24,6 @@ simple_deploy () {
     docker build -t neutrino .
     docker stop neutrino
     docker rm neutrino
-    docker rmi neutrino
     docker run -itd --name neutrino -p 5001:5001 -p 5000:5000 --env-file .env neutrino
 }
 
